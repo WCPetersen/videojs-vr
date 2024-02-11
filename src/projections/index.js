a701b91b910d5bc7d509e91157bd5c75c74320e0
Will Petersen <will@wcpetersen.com>
2024-02-11 13:02:02 -0500

    Refactored changeProjection_ to use new projection classes

---
+++ src/projections/index.js
@@ -0,0 +1,19 @@
+import {
+  Sphere360, Sphere360_LR, Sphere360_TB,
+  Sphere180, Sphere180_LR, Sphere180_TB
+} from './spherical.js';
+import { EAC, EAC_LR } from './eac.js';
+import Cube360 from './cube.js';
+
+export default {
+  '360':      Sphere360,
+  '360_LR':   Sphere360_LR,
+  '360_TB':   Sphere360_TB,
+  '180':      Sphere180_LR, // We treat '180' as '180_LR'
+  '180_MONO': Sphere180,
+  '180_LR':   Sphere180_LR,
+  '180_TB':   Sphere180_TB,
+  '360_CUBE': Cube360,
+  'EAC':      EAC,
+  'EAC_LR':   EAC_LR
+};
